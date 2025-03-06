0fd802351569834f3e9821ee9148ee2de5ac0da5
YasmineBelmiro <devyasminebelmiro@gmail.com>
2025-03-06 18:44:54 -0300

    fix: solucionando problema de rota

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { BrowserRouter,  Route, Routes } from "react-router-dom";
 
 import { GlobalContainer } from "./global";
 import About from "./pages/About/About";
 import Projects from "./pages/Projects/projects";
-import Cv from './pages/Cv/Cv'
+import Cv from "./pages/Cv/Cv";
 import Contacts from "./pages/Contacts/Contacts";
 
-import Header from './components/Header/Header'
+import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Outlet />
       <GlobalContainer>
         <Home />
         <Routes>
