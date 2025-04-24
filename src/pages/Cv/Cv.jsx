84c7104f3bfd1fec5f2c0ed00d895bce5a982fcd
Yasmine Belmiro <devyasminebelmiro@gmail.com>
2025-04-24 08:36:50 -0300

    atualização de informações do curriculo

--- src/pages/Cv/Cv.jsx
+++ src/pages/Cv/Cv.jsx
@@ -21,18 +21,24 @@ import { Link } from "react-router-dom";
 const ExperienceList = [
   {
     id: 1,
-    name: "Desenvolvedora Front-end|Projeto de extensão “Desconto Direto 02/2023 - 12/2025",
+    name: "Estagiária em React | Compass UOL 03/2025 - Atual",
+    description:
+      "Desenvolvimento ágil com Scrum e boas práticas de segurança na web. Programação com JavaScript e TypeScript, versionamento eficiente de código. Construção de aplicações escaláveis com React, Redux e React Router. Documentação de componentes com Storybook e uso de Design Systems. Exploração de serviços AWS e preparação para certificação AWS.",
+  },
+  {
+    id: 2,
+    name: "Desenvolvedora Front-end | Projeto de extensão Desconto Direto 02/2023 - Atual",
     description:
       "Uma aplicação voltada para a retenção de ofertas locais, com o objetivo de tornar o acesso a promoções mais fácil e incentivar o comércio local de forma eficiente e acessível. Desempenho um papel de liderança na equipe e atuo como desenvolvedora front-end.",
   },
   {
-    id: 2,
+    id: 3,
     name: "Embaixadora | DIO Campus Expert 03/2024 - 04/2024",
     description:
       "Participei do programa de Embaixadores Universitários da DIO, uma jornada de 6 semanas focada no desenvolvimento de habilidades de liderança e soft skills, com mentorias, desafios e premiações exclusivas. Durante o programa, compartilhei oportunidades e conhecimentos em tecnologia com colegas universitários, ampliando o impacto positivo da DIO enquanto me desenvolvia em uma experiência prática.",
   },
   {
-    id: 3,
+    id: 4,
     name: "Monitora Voluntária | Programação Orientada a Objetos 03/2024 - 07/2024",
     description:
       "Atuei como monitora, auxiliando calouros com dúvidas, atividades práticas e projetos. Ofereci suporte técnico e acadêmico, ajudando a esclarecer conceitos fundamentais, solucionar problemas de programação e desenvolver habilidades práticas de codificação.",
